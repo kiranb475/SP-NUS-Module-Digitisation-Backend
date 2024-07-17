7542de7a23a53bdb0bfd0e1ea7342b73290c7e71
kiranb475 <46086880+kiranb475@users.noreply.github.com>
2024-07-17 23:53:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 const db = require("./models");

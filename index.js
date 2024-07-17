34a4200c708dd381d870a86042c7413d16fdb926
kiranb475 <46086880+kiranb475@users.noreply.github.com>
2024-07-18 01:14:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -80,8 +80,8 @@ app.use("/instructorlog", instructorLogsRouter);
 db.sequelize
   .sync()
   .then(() => {
-    app.listen(process.env.PORT || 80, () => {
-      console.log(`Server running on port ${process.env.PORT || 80}`);
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server running on port ${process.env.PORT || 8080}`);
     });
   })
   .catch((err) => {
